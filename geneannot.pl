053ef5a46b8c95740a870fb4c0bf69622e9b7bb9
Your Name <z5075362@cse.unsw.EDU.AU>
2016-10-25 11:13:06 +1100

    cleaned up for submission

--- geneannot.pl
+++ geneannot.pl
@@ -6,30 +6,32 @@ use LWP::UserAgent;
 use HTTP::Request::Common qw(POST);
 use List::Util qw( min max );
 
-my $user_agent = LWP::UserAgent->new;
-
-$argc = $#ARGV + 1;
+# Option flags
 my $ps = 0;
+my $f = 0;
 
-
-if ($argc < 1){
+if (($#ARGV + 1) < 1){ # Print usage 
     print "usage: geneannot.pl query\n";
     exit;
-} else {
-
+} else { # Check options
     foreach my $option (@ARGV){
-        if ($option =~ /^-ps/gi){ 
-            shift @ARGV;
+        if ($option =~ /^-ps/gi){ # Run gff output through gff2ps to get ps output
             $ps = 1;
         }
-    
+        
+        if ($option =~ /^-f/gi){ # Collect protein sequences of BLASTx hits in the fasta format
+            $f = 1;
+        }
+        
+        if ($option =~ /^-h/gi){ # Print help information
+            print "=== Geneannotate ===\n Take a sequence in the fasta format and reutrn a gff file with associated ORF's and BLASTx matches\n\n -h => Print this help page\n -ps => Use gff2ps on gff output produced byu this script\n -f => Collect protein sequences of BLASTx hits in the fasta format\n\n";
+            exit 0;                   
+        }
+        shift @ARGV;
     }
-
-
-
 }
 
-
+my $user_agent = LWP::UserAgent->new;
 my $query_seq = $ARGV[0];
 my $query_name = (split /\./, $query_seq)[0];
 my $query_id = "";
@@ -44,7 +46,7 @@ system("getorf -sequence $query_seq -outseq $query_name.gff -osformat2 gff3
 
 open(ORF, "<$query_name.gff") or die "Can't open getorf output";
 	
-# parse ORF file    
+# Parse ORF file    
 while(my $line = <ORF>){
     if($line =~ /^>/){
         # Set index offset for (REVERSE STRING)
@@ -63,30 +65,23 @@ while(my $line = <ORF>){
             $end = $tmp;
         }
 
-        # No type 
         $result = $result."\tORF\t.\t$start\t$end\t.";
         $result = ($line =~ /reverse/i) ? $result."\t-" : $result."\t+";
         $result = $result."\t.\t$query_seq"; 
     
         print OUTPUT "$result\n" or die "File not open";
-
     }
 }
 
 close(ORF);
 
-# do web blast => ./web_blast.pl blastx swissprot geobacter.fasta
-# maybe make this a function?
-
-my $encoded_query = "";
-
-#foreach $query (@ARGV){ # possible extra feature => do stuff on multiple sequences
-    open(QUERY,$query_seq);
-    while(<QUERY>){
-        $encoded_query = $encoded_query . uri_escape($_);
-    } 
-    close(QUERY);
-#}
+# Run web BLAST with BLASTx on Swissprot
+my $encoded_query = "";    
+open(QUERY,$query_seq);
+while(<QUERY>){
+    $encoded_query = $encoded_query . uri_escape($_);
+} 
+close(QUERY);
 
 # build the request
 my $args = "CMD=Put&PROGRAM=blastx&DATABASE=swissprot&QUERY=" . $encoded_query;
@@ -111,25 +106,19 @@ my $rtoe=$1;
 # wait for search to complete
 sleep $rtoe;
  
-# poll for results
+# poll for results => Based on code from https://blast.ncbi.nlm.nih.gov/docs/web_blast.pl
 while (0){ # while true:
-    sleep 5;
+    #sleep 5;
     
     $req = new HTTP::Request GET => "https://blast.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&F    ORMAT_OBJECT=SearchInfo&RID=$rid";
     $response = $user_agent->request($req);
-    if ($response->content =~ /\s+Status=WAITING/m){
+    if ($response->content =~ /Status=WAITING/m){
         print "Searching...\n";
         next;
     }
- 
-    if ($response->content =~ /\s+Status.+searching/im){
-        print "Searching...\n";
-        next;
-    }
- 
  
     if ($response->content =~ /\s+Status=FAILED/m){
-        print STDERR "Search $rid failed; please report to blast-help\@ncbi.nlm.nih.gov.\n";
+        print "Search $rid failed; please report to blast-help\@ncbi.nlm.nih.gov.\n";
         exit 4;
     }
   
@@ -140,10 +129,10 @@ while (0){ # while true:
  
     if ($response->content =~ /\s+Status=READY/m){
         if ($response->content =~ /\s+ThereAreHits=yes/m){
-            #  print STDERR "Search complete, retrieving results...\n";
+            print "Search complete, retrieving results...\n";
             last;
         } else {
-             print STDERR "No hits found.\n";
+             print "No hits found.\n";
              exit 2;
         }
     }
@@ -159,8 +148,9 @@ $response = $user_agent->request($req);
 
 # parse out BLAST results to GFF output
 my $result = "";
-my @query = ();
-my @subject = ();
+my @query = (); # Query sequence indexes
+my @subject = (); # Subject sequence indexes
+my $subject_seq = "";
 my $first_loop = 1; #HACK
 
 
@@ -170,46 +160,46 @@ foreach my $line (split(/\n/, $response->content)){
     my @line_split = split(/ +|, +/, $line);
 
     if ($line =~ /^>/gi){
-    
         my $e_val = (split(/ /, $result))[2];
-        #print "$e_val\n";   #if not ($result eq ""){#or (split(/ /, $result))[2] gt 1e-3){
         
-        if ($e_val < 1e-3){
-            # print result to GFF output
+        if ($e_val < 1e-3){ # Print result to GFF output
             $result = "$result ". min(@query) . " " . max(@query) . " " . min(@subject) . " " . max(@subject);
-            #my $out = $query_id . "\t" . "BLASTX\tEST_match\t" . min(@query) . "\t" . max(@query) . "\t" .   
             my @result_split = split(/ /, $result);      
             my @out = ();
             push @out, ($query_id, "BLASTx", "EST_match", min(@query), max(@query), $result_split[1], $result_split[3], ".", "target=$result_split[0] " . min(@subject) . " " . max(@subject));
-            #print "@out\n";
             
             $result = join("\t", @out);
             print OUTPUT "$result\n" if ($first_loop == 0); # HACK
+            $subject_seq =~ s/\n//g;
+            print OUTPUT "###FASTA\n>$result_split[0]\n$subject_seq\n" if ($f == 1);
             $first_loop = 0; # HACK
         }
         
-        # reset results everytime
+        # Reset results everytime there is a new match
         ($result = $line_split[0]) =~ s/>//gi;
         @query = ();
         @subject = ();
+        $subject_seq = "";
     } elsif ($line =~ /score/gi){
         $result = "$result $line_split[3] $line_split[8]";
-    } elsif ($line =~ /^query /gi){
-        #print "===> $line_split[1] $line_split[3]";
+    } elsif ($line =~ /^query /gi){ # Collect query indexes
         push(@query, $line_split[1]);     
         push(@query, $line_split[3]);
-    } elsif ($line =~ /sbjct /gi){
-        #print "<=== $line_split[1] $line_split[3]";
+    } elsif ($line =~ /sbjct /gi){ # Collect subject indexes
         push(@subject, $line_split[1]);
+        $subject_seq = $subject_seq . $line_split[2];
         push(@subject, $line_split[3]);       
-    } elsif ($line =~ /frame/gi){
+    } elsif ($line =~ /frame/gi){ # Parse strand
         (my $add = $line_split[3]) =~ s/[0-9]//gi;
         $result = "$result $add";
     }
 }
 
-if ($ps == 1){
-    `gff2ps -v annotate$query_name.gff > annotate$query_name.ps`
+if ($ps == 1){ # Execute ps optional action
+    `gff2ps -v annotate$query_name.gff 1> annotate$query_name.ps 2> STDIN`
 }
+
+# Remove intermediary files
+`rm $query_name.gff STDIN`;
 
 close(OUTPUT);
